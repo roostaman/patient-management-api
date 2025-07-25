dadde8c4f69a91552c71be17813bfbd525f2d502
Rustam Akhmetzhanov <RystamAh@halykbank.kz>
2025-07-25 12:03:02 +0500

    chore: add missing dependencies

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,10 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+
+}
