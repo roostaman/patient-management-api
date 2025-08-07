4bf6ea673a9fde998e75de04c44bc622564e64c8
Rustam Akhmetzhanov <RystamAh@halykbank.kz>
2025-08-07 10:45:31 +0500

    chore(kafka): initial Kafka setup with KafkaProducer and proto definition

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.pm.patientservice.kafka;
+
+import com.pm.patientservice.model.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+
+    }
+}
