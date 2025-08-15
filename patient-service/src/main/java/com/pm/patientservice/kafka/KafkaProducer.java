e64f29635d1ccfef7253772523b47870e7a25799
Rustam Akhmetzhanov <RystamAh@halykbank.kz>
2025-08-15 16:28:14 +0500

    chore(kafka): implement KafkaProducer

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,13 +1,17 @@
 package com.pm.patientservice.kafka;
 
 import com.pm.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
 @Service
 public class KafkaProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
     @Autowired
@@ -16,6 +20,20 @@ public class KafkaProducer {
     }
 
     public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
 
+        try {
+            kafkaTemplate.send(
+                    "patient",
+                    event.toByteArray()
+            );
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event);
+        }
     }
 }
